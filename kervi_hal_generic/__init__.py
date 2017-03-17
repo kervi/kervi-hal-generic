5514f4329a09999a30e7a8b868c49b81aa522092
Tim Wentzlau <tim@elektron.fo>
2017-03-17 19:35:39 +0000

    update hal handling

--- kervi_hal_generic/__init__.py
+++ kervi_hal_generic/__init__.py
@@ -1,6 +1,5 @@
 from . import gpio
 from . import i2c
-from . import camera_driver
 
 def get_gpio_driver():
     return gpio.GPIODriver()
@@ -9,4 +8,5 @@ def get_i2c_driver(address, bus=0):
     return i2c.I2CDriver(address, bus)
 
 def get_camera_driver(source):
+    from . import camera_driver
     return camera_driver.CameraDriver()
